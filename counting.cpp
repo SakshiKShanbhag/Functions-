d519b951ab5e9668e040e106cada0bdf5e01006e
SAKSHI K SHANBHAG <82280852+SakshiKShanbhag@users.noreply.github.com>
2022-05-13 21:50:33 +0530

    Add files via upload

---
+++ counting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printcounting(int n){
+    for(int i=1;i<=n;i++){
+        cout<<i<<" ";
+    }
+
+
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+    printcounting(n);
+    return 0;
+}
